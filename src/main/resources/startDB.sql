ac1b53d97bee264e86d16488675dcdce21426748
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-01-10 14:32:38 +0100

    nyt script

--- src/main/resources/startDB.sql
+++ src/main/resources/startDB.sql
@@ -2,20 +2,20 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-DROP SCHEMA IF EXISTS `Semester3` ;
+DROP SCHEMA IF EXISTS `harbour` ;
 
 -- -----------------------------------------------------
 -- Schema CA3
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Semester3` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `Semester3` ;
+CREATE SCHEMA IF NOT EXISTS `harbour` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `harbour` ;
 
 -- -----------------------------------------------------
--- Table `Semester3`.`user`
+-- Table `harbour`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Semester3`.`user` ;
+DROP TABLE IF EXISTS `harbour`.`user` ;
 
-CREATE TABLE IF NOT EXISTS `Semester3`.`user` (
+CREATE TABLE IF NOT EXISTS `harbour`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(25) NOT NULL,
   `user_pass` VARCHAR(255) NULL DEFAULT NULL,
@@ -25,11 +25,11 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `Semester3`.`role`
+-- Table `harbour`.`role`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Semester3`.`role` ;
+DROP TABLE IF EXISTS `harbour`.`role` ;
 
-CREATE TABLE IF NOT EXISTS `Semester3`.`role` (
+CREATE TABLE IF NOT EXISTS `harbour`.`role` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `role_name` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`id`))
@@ -38,11 +38,11 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `Semester3`.`user_has_role`
+-- Table `harbour`.`user_has_role`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Semester3`.`user_has_role` ;
+DROP TABLE IF EXISTS `harbour`.`user_has_role` ;
 
-CREATE TABLE IF NOT EXISTS `Semester3`.`user_has_role` (
+CREATE TABLE IF NOT EXISTS `harbour`.`user_has_role` (
   `user_id` INT NOT NULL,
   `role_id` INT NOT NULL,
   PRIMARY KEY (`user_id`, `role_id`),
@@ -50,12 +50,12 @@ CREATE TABLE IF NOT EXISTS `Semester3`.`user_has_role` (
   INDEX `fk_user_has_role_user1_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_user_has_role_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `Semester3`.`user` (`id`)
+    REFERENCES `harbour`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_has_role_role1`
     FOREIGN KEY (`role_id`)
-    REFERENCES `Semester3`.`role` (`id`)
+    REFERENCES `harbour`.`role` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
